cb0725b0eddf367e7eb949a4eb593e5b1e928f22
aras armani <aras.armani2014@gmail.com>
2019-08-18 20:39:28 +0300

    fixed koso sher neveshti hajiiiii

--- src/App.js
+++ src/App.js
@@ -4,42 +4,28 @@ import { Router ,  Route , browserHistory , IndexRoute  } from 'react-router';
 import './App.css';
  
 
-
+// component root ----->
 import RootComponent from './root';
 import HomeScreen from './container/home/homeComponent';
 import SearchScreen from './container/search/searchComponent';
 import DownloadAppScreen from './container/downloadApp/downloadAppComponent';
 import AboutUsScreen from './container/aboutUs/aboutUsComponent';
 import BebelloScreen from './container/bebello/bebelloComponent';
-
-// import Download from './pages/DownloadApp.jsx';
-// import About from './pages/AboutUs.jsx';
-// import Bebello from './pages/Bebello.jsx';
-// import Search from './pages/Search.jsx';
-// import crosel from './pages/crosel.jsx';
+ 
 
 
 class App extends Component {
   render() {
-    return (
- 
+    return ( 
       <Router history={browserHistory}>
-        <Route  component={RootComponent}> 
-   
-          <Route  path="/" component={HomeScreen} />
-          <Route  path="/home" component={HomeScreen} />
-          <Route  path="/search" component={SearchScreen} />
-          <Route  path="/download" component={DownloadAppScreen} />
-          <Route  path="/about-us" component={AboutUsScreen} />
-          <Route  path="/bebello" component={BebelloScreen} />
-          {/*  
-          <Route  path="/Bebello" component={Bebello} />
-          <Route  path="/Download" component={Download} />
-          <Route  path="/About" component={About} />
-          <Route  path="/Search" component={Search} /> */}
-
-        </Route>
-
+          <Route  component={RootComponent}> 
+              <Route  path="/" component={HomeScreen} />
+              <Route  path="/home" component={HomeScreen} />
+              <Route  path="/search" component={SearchScreen} />
+              <Route  path="/download" component={DownloadAppScreen} />
+              <Route  path="/about-us" component={AboutUsScreen} />
+              <Route  path="/bebello" component={BebelloScreen} />
+          </Route>
       </Router>
     );
   }
