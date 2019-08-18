b35d785b80c228fa71cf7ff30d233e7b718b01e1
aras armani <aras.armani2014@gmail.com>
2019-08-18 20:32:48 +0300

    fixed about us screen is added

---
+++ src/components/aboutUs/aboutUsComponent.js
@@ -0,0 +1,24 @@
+import React,{ Component} from 'react'; 
+
+
+class AboutUsComponent extends Component {
+
+    render()
+    { 
+        return(
+
+            <div> 
+               <div className="container">
+                    <div className="card">
+                        <div className="card-body">
+                            Welcome to About Component
+                        </div>
+                    </div>
+               </div> 
+        </div>
+        );
+    }
+}
+
+
+export default AboutUsComponent;
