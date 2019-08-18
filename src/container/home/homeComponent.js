2fb1bbdb6a3efff2743d9f53ed6de2fc22eab256
aras armani <aras.armani2014@gmail.com>
2019-08-18 17:53:24 +0300

    fixe home is added

---
+++ src/container/home/homeComponent.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import './style.css';
+
+
+
+
+class HomeComponent extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                <p>Home Compoenent</p>
+            </div>
+         );
+    }
+}
+ 
+export default HomeComponent;
